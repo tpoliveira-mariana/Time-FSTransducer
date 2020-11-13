df9868007c75b1b00bf064215bde1df496382d06
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2020-11-13 09:58:07 +0000

    Added fst command for text2num transducer.

--- run.sh
+++ run.sh
@@ -10,18 +10,21 @@ done
 
 # TODO 
 
-
-for i in compiled/*.fst; do
-	echo "Creating image: images/$(basename $i '.fst').pdf"
-    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
-done
-
 echo "Testing the transducer 'converter' with the input 'tests/numero.txt'"
 fstcompose compiled/numero.fst compiled/converter.fst | fstshortestpath | fstproject --project_output | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./syms.txt
 
 echo "Testing the tranducer 'horas' with the input 'tests/horas_1.txt'"
 fstcompose compiled/horas_1.fst compiled/horas.fst compiled/tested_horas.fst
 
-
 echo "Testing the tranducer 'minutos' with the input 'tests/minutos_1.txt'"
 fstcompose compiled/minutos_1.fst compiled/minutos.fst compiled/tested_minutos.fst 
+
+echo "Building transducer text2num from 'horas', 'aux_:' and 'minutos'."
+fstconcat compiled/horas.fst compiled/aux_:.fst > compiled/horas_:.fst
+fstconcat compiled/horas_:.fst compiled/minutos.fst > compiled/text2num.fst
+
+
+for i in compiled/*.fst; do
+	echo "Creating image: images/$(basename $i '.fst').pdf"
+    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
+done
