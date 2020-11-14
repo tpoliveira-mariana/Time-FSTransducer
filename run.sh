fdd84afc7eda32d6bd1702c5d79bc1f3d9a53584
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2020-11-14 16:00:10 +0000

    Added rich2num transducer.

--- run.sh
+++ run.sh
@@ -10,26 +10,21 @@ done
 
 # TRANSDUCERS
 
-echo "Building transducers..."
+echo -e "\nBuilding transducers..."
 
-echo "text2num -> horas + aux_: + e_aux + minutos"
+echo "  text2num -> horas + aux_: + e_aux + minutos"
 fstconcat compiled/horas.fst compiled/aux_:.fst > compiled/horas_:.fst 
-fstrmepsilon compiled/horas_:.fst compiled/horas_:.fst 
-
 fstconcat compiled/aux_e.fst compiled/minutos.fst > compiled/e_minutos.fst
-fstrmepsilon compiled/e_minutos.fst compiled/e_minutos.fst
-
 fstconcat compiled/horas_:.fst compiled/e_minutos.fst > compiled/text2num.fst
 fstrmepsilon compiled/text2num.fst compiled/text2num.fst
 
-echo "lazy2num -> horas_: + aux_00 + text2num"
+echo "  lazy2num -> horas_: + aux_00 + text2num"
 fstunion compiled/e_minutos.fst compiled/aux_00.fst  > compiled/minutos_00.fst
 fstrmepsilon compiled/minutos_00.fst compiled/minutos_00.fst
-
 fstconcat compiled/horas_:.fst compiled/minutos_00.fst > compiled/lazy2num.fst
 fstrmepsilon compiled/lazy2num.fst compiled/lazy2num.fst
 
-echo "rich2text -> horas + aux_e + quartos + meias"
+echo "  rich2text -> horas + aux_e + quartos + meias"
 fstproject compiled/horas.fst > compiled/horas_text.fst
 fstrmepsilon compiled/horas_text.fst compiled/horas_text.fst
 fstproject compiled/aux_e.fst > compiled/e_text.fst
@@ -39,6 +34,13 @@ fstconcat compiled/horas_e_text.fst compiled/quartos.fst > compiled/horas_e_quar
 fstunion compiled/horas_e_meia.fst compiled/horas_e_quarto.fst > compiled/rich2text.fst
 fstrmepsilon compiled/rich2text.fst compiled/rich2text.fst
 
+echo "  rich2num -> horas_e_meia + horas_e_quarto + lazy2num"
+fstunion compiled/horas_e_meia.fst compiled/horas_e_quarto.fst > compiled/treated_time.fst
+fstarcsort compiled/treated_time.fst compiled/treated_time.fst
+fstarcsort compiled/lazy2num.fst compiled/lazy2num.fst
+fstcompose compiled/treated_time.fst compiled/lazy2num.fst > compiled/treated2num.fst
+fstunion compiled/treated2num.fst compiled/lazy2num.fst > compiled/rich2num.fst
+fstrmepsilon compiled/rich2num.fst compiled/rich2num.fst
 
 # TESTS
 echo -e "\nTesting transducers..."
@@ -74,6 +76,23 @@ fstcompose compiled/rich2text_1.fst compiled/rich2text.fst compiled/tests/tested
 
 echo -e "\tinput: tests/rich2text_2.txt"
 fstcompose compiled/rich2text_2.fst compiled/rich2text.fst compiled/tests/tested_rich2text_2.fst 
+
+echo "  -> rich2num:"
+echo -e "\tinput: tests/lazy2num_1.txt"
+fstcompose compiled/lazy2num_1.fst compiled/rich2num.fst compiled/tests/tested_rich2num_1.fst 
+
+echo -e "\tinput: tests/lazy2num_2.txt"
+fstcompose compiled/lazy2num_2.fst compiled/rich2num.fst compiled/tests/tested_rich2num_2.fst 
+
+echo -e "\tinput: tests/text2num_1.txt"
+fstcompose compiled/text2num_1.fst compiled/rich2num.fst compiled/tests/tested_rich2num_3.fst 
+
+echo -e "\tinput: tests/rich2num_1.txt"
+fstcompose compiled/rich2num_1.fst compiled/rich2num.fst compiled/tests/tested_rich2num_4.fst 
+
+echo -e "\tinput: tests/rich2num_2.txt"
+fstcompose compiled/rich2num_2.fst compiled/rich2num.fst compiled/tests/tested_rich2num_5.fst 
+
 
 
 # IMAGES
